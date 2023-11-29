4544d42bfba72fae02785855ae5cc4d05ad0041b
annabrisland <84166964+annabrisland@users.noreply.github.com>
2023-11-29 11:54:42 -0800

    Dynamically populates modal name & time

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -1,11 +1,21 @@
 var resultsSection = $("#results");
+var modalEventName = $("#event-name");
+var modalTime = $("#modal-time");
+
 // test event name
 var eventName = "Test event #1"
 
 // Function to populate modal
 function populateModal() {
-    console.log("hi")
-    
+    modalEventName.text(eventName);
+
+    // Loop to create time options in modal
+    for (var i = 0; i < 24 ; i++) {
+        var timeSlot = $("<option>");
+        var time = i.toString().padStart(2, "0") + ":00";
+        timeSlot.text(time);
+        modalTime.append(timeSlot);
+    }
 }
 
 // Function to add event
@@ -15,11 +25,11 @@ function addEvent() {
 
 // Event listener for card add button
 resultsSection.on("click", "button.add-btn", function() {
+    // On click, populate modal
     populateModal();
     
 });
 
 
 // Each card has ID with the name of event
-// On click, populate modal
-// For loop for times in modal
+
