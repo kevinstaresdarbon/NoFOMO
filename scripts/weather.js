001e4b1d8d146085c2a4a5a733cca13d1dcafcf8
Janeks111 <90975618+Janeks111@users.noreply.github.com>
2023-12-04 19:52:55 +0000

    Fixing the error in the console (locantin not found) and bug that doubles all the weather when search differen event type

--- scripts/weather.js
+++ scripts/weather.js
@@ -33,19 +33,21 @@ function updateWeatherForCity(city) {
         // Update time card with temperature and weather icon
         const taskBox = document.getElementById(`hour-${hour}`);
 
+        // Clear existing content
+        taskBox.innerHTML = "";
+
         // Convert temperature from Kelvin to Celsius
         const temperatureCelsius = Math.round(temperature - 273.15);
 
-        // Prepend weather info
+        // Append weather info
         var weatherEl = document.createElement("div");
         var weatherImg = document.createElement("img");
-        weatherImg.setAttribute("src", "https://openweathermap.org/img/wn/" + `${weatherIcon}` + ".png");
+        weatherImg.setAttribute(
+          "src",
+          "https://openweathermap.org/img/wn/" + `${weatherIcon}` + ".png"
+        );
         weatherEl.append(`${temperatureCelsius}` + "°C", weatherImg);
-        console.log(weatherImg)
         taskBox.append(weatherEl);
-        // taskBox.prepend(`<p>${temperatureCelsius}°C</p>
-        // <img src="https://openweathermap.org/img/wn/${weatherIcon}.png" alt="Weather Icon">
-        // `);
       });
     },
     function (error) {
@@ -58,17 +60,15 @@ function updateWeatherForCity(city) {
 function updateWeatherForInputLocation() {
   const inputLocation = $("#locationInput").val();
 
-  if (!inputLocation) {
-    console.error("Input location is empty. Please provide a valid location.");
-    return;
+  if (inputLocation) {
+    console.log("Updating weather for location:", inputLocation);
+    updateWeatherForCity(inputLocation);
+  } else {
+    console.warn("Input location is empty. Please provide a valid location.");
   }
-
-  console.log("Updating weather for location:", inputLocation);
-
-  updateWeatherForCity(inputLocation);
 }
 
 // Example usage: Update weather using the location from the input field
 $(document).ready(function () {
-  updateWeatherForInputLocation();
+  $("#searchBtn").on("click", updateWeatherForInputLocation);
 });
