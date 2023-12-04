b38da4f8adc694c9a841c870a986449b14f6049f
Janeks111 <90975618+Janeks111@users.noreply.github.com>
2023-12-04 20:05:28 +0000

    attempt to fix the issue with schedule not adding to the time cards

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -4,50 +4,55 @@ var modalTime = $("#modal-time");
 
 // Function to populate modal
 function populateModal(eventName, eventSrc) {
-    modalEventName.text(eventName);
-    modalEventName.attr("name", eventName);
-    modalEventName.attr("url-src", eventSrc);
+  modalEventName.text(eventName);
+  modalEventName.attr("name", eventName);
+  modalEventName.attr("url-src", eventSrc);
 
-    // Loop to create time options in modal
-    for (var i = 0; i < 24 ; i++) {
-        var timeSlot = $("<option>");
-        timeSlot.attr("value", i);
-        var time = i.toString().padStart(2, "0") + ":00";
-        timeSlot.text(time);
-        modalTime.append(timeSlot);
-    }
+  // Loop to create time options in modal
+  for (var i = 0; i < 24; i++) {
+    var timeSlot = $("<option>");
+    timeSlot.attr("value", i);
+    var time = i.toString().padStart(2, "0") + ":00";
+    timeSlot.text(time);
+    modalTime.append(timeSlot);
+  }
 }
-
-
 
 // Function to add event
 function addEvent(eventName, eventSrc) {
-    // set variables for time, and duration
-    var eventTime = modalTime.val();
-    var eventDuration = "Duration: " + $("#modal-duration").val();
+  // set variables for time, and duration
+  var eventTime = modalTime.val();
+  var eventDuration = "Duration: " + $("#modal-duration").val();
 
-    // Add to schedule
-    var timeID = "#task-" + eventTime;
-    var taskName = $("<a>").attr({href: eventSrc, target: "_blank"}).text(eventName);
-    $(timeID).append(taskName, eventDuration);
-    // Adds background colour to schedule block once item is added
-    $(timeID).css("background-color", "red");
+  // Add to schedule
+  var timeID = "#task-" + eventTime;
+
+  // Create a new task box for the event
+  var taskBox = $("<div>").addClass("task-box");
+  var taskName = $("<a>")
+    .attr({ href: eventSrc, target: "_blank" })
+    .text(eventName);
+  taskBox.append(taskName, eventDuration);
+
+  // Append the task box to the correct time slot
+  $(timeID).find(".task").append(taskBox);
+
+  // Adds background color to schedule block once item is added
+  $(timeID).css("background-color", "red");
 }
 
 // Event listener for card add button
-resultsSection.on("click", "button.add-btn", function() {
-    var eventName = $(this).parent().siblings("h2").attr("card-title");
-    var eventSrc = $(this).siblings().attr("data-viewsrc");
-    // On click, populate modal
-    populateModal(eventName, eventSrc);
-    
+resultsSection.on("click", "button.add-btn", function () {
+  var eventName = $(this).parent().siblings("h2").attr("card-title");
+  var eventSrc = $(this).siblings().attr("data-viewsrc");
+  // On click, populate modal
+  populateModal(eventName, eventSrc);
 });
 
 // Event listener for modal submission
-$("#schedule-btn").on("click", function() {
-    var eventName = modalEventName.attr("name");
-    var eventSrc = modalEventName.attr("url-src");
-    // Create schedule event
-    addEvent(eventName, eventSrc);
-
-})
+$("#schedule-btn").on("click", function () {
+  var eventName = modalEventName.attr("name");
+  var eventSrc = modalEventName.attr("url-src");
+  // Create schedule event
+  addEvent(eventName, eventSrc);
+});
